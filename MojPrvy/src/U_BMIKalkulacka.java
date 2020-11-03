8c423d81e1e2971126fbceebfd306e8416abf2dc
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2020-11-03 12:55:07 +0100

    ulohy vetvenie + vstup

---
+++ MojPrvy/src/U_BMIKalkulacka.java
@@ -0,0 +1,17 @@
+/**
+ * Kalkulacka pre vypocet hodnoty BMI na zaklade zadanej hmotnosti
+ * a vysky
+ * @author lukas
+ *
+ */
+public class U_BMIKalkulacka {
+
+	public static void main(String[] args) {
+		//Vstup: hmotnost[kg], vyska[cm]
+		//Vystup: hodnota BMI + info
+		//->86, 186
+		//<- BMI = 24.9 - normalna a zdrava vaha
+
+	}
+
+}
---
+++ MojPrvy/src/U_KvadratickaRovnica.java
@@ -0,0 +1,14 @@
+/**
+ * Vypocet korenov kvadratickej rovnice
+ * @author lukas
+ *
+ */
+public class U_KvadratickaRovnica {
+
+	public static void main(String[] args) {
+		//Vstup: a, b, c
+		//Vystup: korene kvadr. rovnice / nema riesenie
+
+	}
+
+}
