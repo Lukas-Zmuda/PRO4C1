bdfdf831b7107332fa7816918bcf9616d29643c4
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-02-16 12:36:43 +0100

    oop zaklady - konstruktor

---
+++ OOP/src/Tester.java
@@ -0,0 +1,33 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		//deklaracia a inicializacia objektu
+		Ziak z = new Ziak();
+		
+		z.meno = "Jan";
+		z.priezvisko = "Hrasko";
+		z.trieda = "1.A";
+		
+		Ziak z2 = new Ziak();
+		z2.meno = "Fero";
+		
+		System.out.println(z.celeMeno());
+		//System.out.println(z2.celeMeno());
+		
+		z.info();
+		
+		z.zmenTriedu("3.A");
+		z.info();
+		z2.info();
+		
+		Ziak z3 = new Ziak("Peter", "Pan", "1.C");
+		z3.info();
+		
+		Ziak z4 = new Ziak("Jan", "Hus");
+		z4.info();
+		
+	}
+
+}
---
+++ OOP/src/Ziak.java
@@ -0,0 +1,53 @@
+class Ziak {
+
+	//instancne premenne - fields
+	String meno;
+	String priezvisko;
+	String trieda;
+	
+	//konstruktor
+	Ziak(){
+		meno = "nezname";
+		priezvisko = "nezname";
+		trieda = "neznama";
+	}
+	
+	Ziak(String meno, String priezvisko, String trieda){
+		this.meno = meno;
+		this.priezvisko = priezvisko;
+		this.trieda = trieda;
+	}
+	
+	Ziak(String meno, String priezvisko){
+		this(meno, priezvisko, "nepriradena");
+	}
+	
+	//metody - schopnosti objektu
+	
+	/**
+	 * Metoda vrati cele meno ziaka
+	 * @return cele meno
+	 */
+	String celeMeno() {
+		return meno + " " + priezvisko;
+	}
+	
+	/**
+	 * Metoda vypise kompletne informacie o ziakovi na konzolu:
+	 * Meno a priezvisko:
+	 * Trieda:
+	 */
+	void info() {
+		System.out.println("Meno a priezvisko: " + celeMeno());
+		System.out.println("Trieda: " + trieda);
+	}
+	
+	/**
+	 * Metoda zmeni triedu ziaka
+	 * @param novaTrieda
+	 */
+	void zmenTriedu(String novaTrieda) {
+		trieda = novaTrieda;
+	}
+	
+}
